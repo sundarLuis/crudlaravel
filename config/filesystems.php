b0024cc584d04e1ceceeaa47dc21bc5efffb8ea0
Luis Sundar <sundarjuegos@gmail.com>
2019-04-26 09:56:36 -0500

    sexto commit

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('base64:D52fF2E5D9awITzk1HFPNB5Wuh0E7i0i8aVHIBwkM7w=').'/storage',
             'visibility' => 'public',
         ],
 
